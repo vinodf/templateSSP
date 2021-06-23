ffcde98a8021a19f8df1c163589e2d86e4fdd986
Valdevino Peixoto <vinodf@gmail.com>
2021-06-23 13:43:54 -0300

    Adding navbar and topbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
 
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent,
+    TopbarComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule
+  ],
+  exports:[
+    // shared modules
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+    // shared components
+    NavbarComponent,
+    TopbarComponent
   ]
 })
 export class CoreModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { FormFieldErrorComponent } from './components/form-field-error/form-field-error.component';
+import { ServerErrorMessagesComponent } from './components/server-error-messages/server-error-messages.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BreadCrumbComponent,
+    PageHeaderComponent,
+    FormFieldErrorComponent,
+    ServerErrorMessagesComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule
+  ],
+  exports: [
+    // shared modules
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+
+    // shared components
+    BreadCrumbComponent,
+    PageHeaderComponent,
+    FormFieldErrorComponent,
+    ServerErrorMessagesComponent
   ]
 })
 export class SharedModule { }
